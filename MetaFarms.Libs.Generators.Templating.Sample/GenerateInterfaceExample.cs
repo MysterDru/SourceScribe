52d8522ba045ff2d9a73a23528375d16b17fd4df
Drew Frisk <dfrisk@metafarms.com>
2023-12-18 08:44:18 -0600

    method generation for interface

--- MetaFarms.Libs.Generators.Templating.Sample/GenerateInterfaceExample.cs
+++ MetaFarms.Libs.Generators.Templating.Sample/GenerateInterfaceExample.cs
@@ -1,41 +1,82 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using MetaFarms.Libs.Generators.Templating;
 
 namespace MetaFarms.Libs.Templating.Sample;
 
+/// <summary>
+/// Class that demonstrates the generation of an interface
+/// </summary>
 [TypeMemberTemplate("InterfaceGenerator.scriban")]
 public partial class GenerateInterfaceExample : IGenerateInterfaceExample
 {
     private List<string> _foo = new (); 
     
-    // public string X { get; init; }
+    /// <summary>
+    /// Indexer with single arg
+    /// </summary>
+    /// <param name="i"></param>
     public string this[int i]
     {
         get => _foo[i];
         set => _foo[i] = value;
     }
     
+    /// <summary>
+    /// indexer with multiple args
+    /// </summary>
+    /// <param name="i"></param>
+    /// <param name="args"></param>
     public string this[int i, string[] args]
     {
         get => _foo[i];
         set => _foo[i] = value;
     }
     
+    /// <summary>
+    /// String property
+    /// </summary>
     public string Foo { get; set; }
     
+    /// <summary>
+    /// Get and set, <see cref="Nullable{T}"/>
+    /// </summary>
     public int? Bar { get; set; }
     
+    /// <summary>
+    /// Getter only
+    /// </summary>
     public object Fizz { get; }
     
+    /// <summary>
+    /// Private getter, public setter
+    /// </summary>
     public string Buzz { private get; set; }
  
+    /// <summary>
+    /// so something, no params
+    /// </summary>
     public void DoSomething()
     {
     }
 
+    /// <summary>
+    /// Do something async
+    /// </summary>
+    /// <returns><see cref="Task{TResult}"/></returns>
     public Task DoSomethingAsync()
     {
         return Task.CompletedTask;
+    }
+    
+    /// <summary>
+    /// Do something
+    /// </summary>
+    /// <param name="foo"></param>
+    /// <returns></returns>
+    public Task<string> DoSomethingAsync(string foo)
+    {
+        return Task.FromResult(foo);
     }
 }
